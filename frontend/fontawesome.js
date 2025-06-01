5ced862de41ff3b74c6e5106b7c95c32dccc090d
CCoupse <juliantijulianti39@gmail.com>
2025-06-01 11:21:30 +0700

    Delete frontend directory

--- frontend/fontawesome.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const style = document.createElement('style');
-  style.innerHTML = `
-    .fas.fa-map-marker-alt::before { content: "\\f3c5"; }
-    .fas.fa-search::before { content: "\\f002"; }
-    .fas { font-family: "FontAwesome"; }
-  `;
-  document.head.appendChild(style);
-})();
